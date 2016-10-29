aed44940286e138d7292eb0c67089f31317d8ca7
rquintao <rafaelerquintao@gmail.com>
2016-10-29 23:34:36 +0100

    Primeiro commit

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<link rel="stylesheet" type="text/css" href="Stylesheets/header.css">
+	
+	<div style="position: relative; left: 0; top: 0;">
+  		
+  		<img src="Resources/headerBackground.jpg" class="headerBackground"/>
+
+  		<img src="Resources/headerLogo.png" class="headerLogo"/>
+
+  		<input type = "text"
+                 id = "utilizadorInput"
+                 value="" />
+
+        <input type = "password"
+                 id = "passwordInput"
+                 value="" />
+
+        <h2 id="utilizador">Utilizador</h2>
+        <h2 id="password">Password</h2>
+
+	</div>
+	
+</head>
+
+
+<body>
+
+<h3>TESTE</h3>
+
+<?php
+ include ('teste.php');
+?>
+asd
+</body>
+</html>
