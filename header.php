37a290c089dd9f8cc4f1c582538a4630a2b8a59d
Grifo07 <egrifo@gmail.com>
2016-10-30 01:38:13 +0000

    Mudado o header

--- header.php
+++ header.php
@@ -1,39 +1,87 @@
 <!DOCTYPE html>
 <html>
 
-<head>
+  <head>
 
-	<link rel="stylesheet" type="text/css" href="Stylesheets/header.css">
-	
-	<div style="position: relative; left: 0; top: 0;">
-  		
-  		<img src="Resources/headerLogo.png" class="headerLogo"/>
+  	<title>RG Peças</title>
+		<link rel="stylesheet" type="text/css" href="Stylesheets/header.css">
+		<!-- 
+  	<link rel="stylesheet" type="text/css" href="Stylesheets/header.css">
+  	
+  	<div style="position: relative; left: 0; top: 0;">
+      
+      <img src="Resources/headerLogo.png" class="headerLogo"/>
 
-  		<input type = "text"
-                 id = "utilizadorInput"
-                 value="" />
+      <input type = "text"
+      id = "utilizadorInput"
+      value="" />
 
-        <input type = "password"
-                 id = "passwordInput"
-                 value="" />
+      <input type = "password"
+      id = "passwordInput"
+      value="" />
 
-        <h2 id="utilizador">Utilizador</h2>
-        <h2 id="password">Password</h2>
+      <h2 id="utilizador">Utilizador</h2>
+      <h2 id="password">Password</h2>
 
-	</div>
-	
-</head>
-
-
+    </div>
+     -->
+  </head>
 
 
-<body>
+  <body>
+		<header>
+			<div class="container">
 
-<h3>TESTE</h3>
+      		<img src="Resources/headerLogo.png" class="headerLogo coluna"/>
 
-<?php
- include ('teste.php');
-?>
-asd
-</body>
+					<div class="coluna aux">
+
+						<div class="linha">
+
+							<div class="coluna" id="loginheader2">
+								
+								<div class="linha">
+									<input type = "text"
+						      id = "utilizadorInput"
+						      value="" />
+								</div>
+								<div class="linha">
+									<input type = "password"
+						      id = "passwordInput"
+						      value="" />
+								</div>
+							</div>      
+							
+							<div class="coluna" id="loginheader1">
+								
+								<div class="linha" id="textologin">
+									<strong>Utilizador</strong>
+								</div>
+								<div class="linha" id="textologin">
+									<strong>Password</strong>
+								</div>
+					      
+							</div>
+
+						</div>
+
+						<div class="linha">
+							<a href="" class="botoesentrada">Entrar</a>
+							<a href="" class="botoesentrada">Registar</a>
+						</div>
+
+					</div>
+
+			</div>
+
+		</header>
+
+
+		<footer>
+			
+
+		</footer>
+    
+  </body>
+
 </html>
