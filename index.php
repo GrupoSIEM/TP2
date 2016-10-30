4d8e4080d2cbce1fac4dc242af27145c0fbaacb6
Grifo07 <egrifo@gmail.com>
2016-10-30 21:46:56 +0000

    Revert "footer adicionado"
    This reverts commit 4308ea018e0287dcd5f78b93ef900c0d0de0b415.

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	
+	<head>
+		<title>RG Peças</title>
+
+	</head>
+
+	<body>
+		<?php include 'header.php';?> <!-- Inclusão do header -->
+
+		
+
+		
+
+
+
+
+
+
+
+
+
+		<?php include 'footer.php';?> <!-- Inclusão do footer -->
+
+	</body>
+
+
+
+
+</html>
