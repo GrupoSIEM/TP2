3142c972fa4b7fbf9c4d11228240dbf40d0922b7
Grifo07 <egrifo@gmail.com>
2016-11-01 00:36:12 +0000

    Avanços no registo

--- DBAccess/user.php
+++ DBAccess/user.php
@@ -1,8 +1,29 @@
 <?php
 
-	
+	function userexists($nome) {
+		include '../TP2/Common/connectdb.php';
 
-	function searchbyname($nome) {
+		$query = "select * from utilizador where nome = '".$nome."';";
+
+
+		$result = pg_exec($conn, $query);
+
+		$row = pg_fetch_row($result, 0); //obter a primeira linha [0]
+		
+		if(isset($row[0])) {
+			$res = true;
+		} else {
+			$res = false;
+		}
+
+
+		pg_close($conn);
+
+		return $res;
+
+	}
+
+	function getuser($nome) {
 		include '../Common/connectdb.php';
 
 		$query = "select * from utilizador where nome = '".$nome."';";
@@ -43,8 +64,25 @@
 
 	}
 
-	
+	function insertuser($username, $nome, $email, $telemovel, $pass) {
+		include '../TP2/Common/connectdb.php';
 
+		// $query1 = "select max(id) from utilizador;";
+		// $result1 = pg_exec($conn, $query1);
+		// $row = pg_fetch_row($result, 0); //obter a primeira linha [0]
+		// $id = $row[0] + 1;
+		$id=2;
+
+		$query2 = "insert into utilizador (id,nome,telefone,nomeusuario, password) values (".$id.",".$nome.",".$telemovel.",".$username.",".$pass.");";
+
+
+		$result2 = pg_exec($conn, $query2);
+
+		pg_close($conn);
+
+		return $result2;
+
+	}
 
 
 ?>
--- signup.php
+++ signup.php
@@ -36,7 +36,7 @@
 				width: 90%;
 			}
 
-			#caixaregemail {
+			#caixaregmaior {
 				height: 20px;
 				width: 95%;
 			}
@@ -79,23 +79,42 @@
 		<?php
 			// define variables and set to empty values
 			$MsgErro = "";
-			$nome = $email = $telemovel = $pass1 = $pass2 = "";
+			$username = $nome = $email = $telemovel = $pass1 = $pass2 = "";
 
 			if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 				do {
-				  if (empty($_POST["reguser"]) || empty($_POST["regemail"]) || empty($_POST["regtelemovel"]) || empty($_POST["regpassword1"]) || empty($_POST["regpassword2"])) {
+				  if (empty($_POST["regusername"]) || empty($_POST["reguser"]) || empty($_POST["regemail"]) || empty($_POST["regtelemovel"]) || empty($_POST["regpassword1"]) || empty($_POST["regpassword2"])) {
 				    $MsgErro = "Preencha todos os campos obrigatórios marcados com *";
 				    break;
 				  } else {
 				    $nome = test_input($_POST["reguser"]);
+				    $username = test_input($_POST["regusername"]);
 				    $email = test_input($_POST["regemail"]);
 				    $telemovel = test_input($_POST["regtelemovel"]);
-				    $pass1 = test_input($_POST["regpassword1"]);
-				    $pass2 = test_input($_POST["regpassword2"]);
+				    $pass1 = $_POST["regpassword1"];
+				    $pass2 = $_POST["regpassword2"];
 				  }
 
-				  // if() {}
+				  if($pass1 != $pass2) {
+				  	$MsgErro = "As Palavras-Chave têm que coincidir";
+				    break;
+				  }
+
+				  $res = userexists($nome);
+
+				  if($res) {
+				  	$MsgErro = "Utilizador já existe";
+				    break;
+				  } else {
+				  	$res2 = insertuser($username, $nome, $email, $telemovel, $pass1);
+				  }
+
+				  if ($res2) {
+						header("www.google.com");
+				  } else {
+						$MsgErro = "Erro ao registar utilizador";
+				  }
 
 
 				} while(0);
@@ -116,39 +135,51 @@
 			<form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 				<table>
 				  <tr>
-				    <th colspan="3">Registo</th>
+				    <th colspan="4">Registo</th>
 				  </tr>
 				  <tr>
-				    <td width="50%"></td><td width="25%"></td><td width="25%"></td>
+				    <td width="50%" colspan="2"></td><td width="25%"></td><td width="25%"></td>
 				  </tr>
 				  <tr>
-				    <td width="50%">Nome:*</td><td width="50%" colspan="2">Email:*</td>
+				    <td width="50%" colspan="2">Nome:*</td><td width="50%" colspan="2">Email:*</td>
 				  </tr>
 				  <tr>
-				    <td width="50%" id="temcaixa">
-				    	<input type = "text" id="caixareg" name = "reguser" value=""></input>
+				    <td width="50%" id="temcaixa" colspan="2">
+				    	<input type = "text" id="caixaregmaior" name = "reguser" value=""></input>
 				    </td>
 				    <td width="50%" id="temcaixa" colspan="2">
-				    	<input type = "text" id="caixaregemail" name = "regemail" value=""></input>
+				    	<input type = "text" id="caixaregmaior" name = "regemail" value=""></input>
 				    </td>
 				  </tr>
 				  <tr>
-				    <td width="50%">Telemóvel:*</td><td width="25%">Palavra-Chave:*</td><td width="25%">Confirmar Palavra-Chave:*</td>
+				    <td width="25%">Username:*</td>
+				    <td width="25%">Telemóvel:*</td>
+				    <td width="25%">Palavra-Chave:*</td>
+				    <td width="25%">Confirmar Palavra-Chave:*</td>
 				  </tr>
 				  <tr>
-				    <td width="50%" id="temcaixa">
+				  	<td width="25%" id="temcaixa">
+				  		<input type = "text" id="caixareg" name = "regusername" value=""></input>
+				  	</td>
+				    <td width="25%" id="temcaixa">
 				    	<input type = "text" id="caixareg" name = "regtelemovel" value=""></input>
-				    </td><td width="25%" id="temcaixa"><input type = "password" id="caixareg" name = "regpassword1" value=""></input></td><td width="25%" id="temcaixa"><input type = "password" id="caixareg" name = "regpassword2" value=""></input></td>
+				    </td>
+				    <td width="25%" id="temcaixa">
+				    	<input type = "password" id="caixareg" name = "regpassword1" value=""></input>
+				    </td>
+				    <td width="25%" id="temcaixa">
+				    	<input type = "password" id="caixareg" name = "regpassword2" value=""></input>
+				    </td>
 				  </tr>
 				  <tr>
-				    <td width="50%"><h4>Morada:</h4></td><td width="25%"></td><td width="25%"></td>
+				    <td width="50%"  colspan="2"><h4>Morada:</h4></td><td width="25%"></td><td width="25%"></td>
 				  </tr>
 				  <tr>
-				    <td width="50%">Endereço:</td><td width="25%">Código Postal:</td><td width="25%">Cidade:</td>
+				    <td width="50%"  colspan="2">Endereço:</td><td width="25%">Código Postal:</td><td width="25%">Cidade:</td>
 				  </tr>
 				  <tr>
-				    <td width="50%" id="temcaixa">
-				    	<input type = "text" id="caixareg" name = "regendereco1" value=""></input>
+				    <td width="50%" id="temcaixa"  colspan="2">
+				    	<input type = "text" id="caixaregmaior" name = "regendereco1" value=""></input>
 				    </td>
 				    <td width="25%" id="temcaixa">
 				    	<input type = "password" id="caixareg" name = "regcodpostal" value=""></input>
@@ -158,11 +189,11 @@
 				    </td>
 				  </tr>
 				  <tr>
-				    <td width="50%"></td><td width="25%">Região:</td><td width="25%">País:</td>
+				    <td width="50%"  colspan="2"></td><td width="25%">Região:</td><td width="25%">País:</td>
 				  </tr>
 				  <tr>
-				    <td width="50%" id="temcaixa">
-				    	<input type = "text" id="caixareg" name = "regendereco2" value=""></input>
+				    <td width="50%" id="temcaixa"  colspan="2">
+				    	<input type = "text" id="caixaregmaior" name = "regendereco2" value=""></input>
 				    </td>
 				    <td width="25%" id="temcaixa">
 				    	<input type = "password" id="caixareg" name = "regregiao" value=""></input>
@@ -172,17 +203,12 @@
 				    </td>
 				  </tr>
 				  <tr>
-				    <td width="50%"><span class="error"><?php echo $MsgErro;?></span></td><td width="25%"></td><td width="25%"><button type = "submit" name="cmdsubmit" value="">Registar</button><button type = "reset" name="cmdreset" value="">Limpar Dados</button></td>
+				    <td width="50%"  colspan="2"><span class="error"><?php echo $MsgErro;?></span></td><td width="25%"></td><td width="25%"><button type = "submit" name="cmdsubmit" value="">Registar</button><button type = "reset" name="cmdreset" value="">Limpar Dados</button></td>
 				  </tr>
 							  
 				</table>
 			</form>
 
-			<?php
-				if (empty($MsgErro)) {
-					
-				} 
-			?>
 			
 		</div>
 
