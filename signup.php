55727e653cfee4d00c670ecce169098c4c0a69e8
Grifo07 <egrifo@gmail.com>
2016-11-03 00:51:08 +0000

    verificação email

--- signup.php
+++ signup.php
@@ -103,6 +103,13 @@
 
 				  //Fazer verificação de email e do numero
 
+				  if(!eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
+				      $MsgErro = "O email não é válido. Por favor introduza um email válido e tente novamente."; // Return Error - Invalid Email
+				      break;
+				  }
+
+				  
+
 				  $res = userexists($nome);
 
 				  if($res) {
