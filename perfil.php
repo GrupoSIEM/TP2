d209962d5db5a79b10871d5c65898ab03628d221
Grifo07 <egrifo@gmail.com>
2016-11-04 17:41:11 +0000

    ss

---
+++ perfil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>PG Pecas</title>
+
+	</head>
+
+
+
+	<body>
+		<?php
+
+			include ('Common/sessioncheck.php');
+
+
+		// serve the page normally.
+
+			include ('Resources/headerlogged.php');
+			include ('Resources/footer.php');
+
+		?>
+
+	</body>
+
+
+</html>
--- pesquisa.php
+++ pesquisa.php
@@ -34,20 +34,19 @@
 
 			<table class="tabelapesquisa">
 				  <tr>
-				    <th width="20%">Imagem</th><th width="20%">Nome</th><th width="20%">Marca</th><th width="20%">Preço</th><th width="20%">Adicionar ao carrinho</th>
-				  </tr>
-					
+					    <th width="20%">Imagem</th><th width="20%">Nome</th><th width="20%">Marca</th><th width="20%">Preço</th><th width="20%">Adicionar ao carrinho</th>
+					  </tr>
 				  <?php $res=verpeças($_POST['caixapesquisa']);
 
 				  	while ($row = pg_fetch_assoc($res))
 					{ 
-					    echo '<tr><td width="20%"><img src="'.$row["link"].'" class="imagens" /> </td><td width="20%">'.$row["nome"].'</td><td width="20%">'.$row["marca"].'</td><td width="20%">'.$row["preço"].' €</td><td width="20%"><img src="../TP2/Resources/cart.png" class="carrinho" /></td></tr>"];';
+					    echo '<tr><td width="20%"><img src="'.$row["link"].'" class="imagens" /> </td><td width="20%">'.$row["nome"].'</td><td width="20%">'.$row["marca"].'</td><td width="20%">'.$row["preço"].' €</td></a><td width="20%"><img src="../TP2/Resources/cart.png" class="carrinho" /></td></tr>';
 
 					} 
 					
 				  ;?>
-				  
-							  
+				  </form>
+				    
 			</table>
 	
 
