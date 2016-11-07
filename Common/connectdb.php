0709a38ef6ab748937cd11c9d7fc883307e50f05
rquintao <rafaelerquintao@gmail.com>
2016-11-07 23:51:11 +0000

    connect

--- Common/connectdb.php
+++ Common/connectdb.php
@@ -1,15 +1,14 @@
 <?php
 
 
-  $conn = pg_connect("host=vdbm.fe.up.pt dbname='sinf15g31' user=sinf15g31 password=grifoquintaotiago");
 
-  //$conn = pg_connect("host=db.fe.up.pt dbname=siem1635 user=siem1635 password=pedro");
+  $conn = pg_connect("host=db.fe.up.pt dbname=siem1639 user=siem1635 password=LmeGrjVv");
   
   if (!$conn) {
     echo "An error occured..\n";
     exit;
 
-    //  $query_conn = 'SET search_path to "siemphp"';  	#define o schema
-	// $connect = pg_exec($conn, $query_conn);			#Selecciona o schema
+     $query_conn = 'SET search_path to "siemphp"';  	#define o schema
+	 $connect = pg_exec($conn, $query_conn);			#Selecciona o schema
   }
 ?>
